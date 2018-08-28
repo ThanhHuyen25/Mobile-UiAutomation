b34ac03e6b7eb7501769d7f711d990616a316019
Bui Chau Anh <buichauanh279@gmail.com>
2018-08-28 16:58:45 +0700

    define action

---
+++ GUI_Testing_Automation_CL/Model/UIElements/Mobile/Android/ButtonAndroidElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Testing_Automation
+{
+    class ButtonAndroidElement:ElementBase
+    {
+        public ButtonAndroidElement(string id) : base(id) { }
+        public ButtonAndroidElement()
+            : base()
+        {
+            //this.Name = name;
+            //this.Children = new List<IElement>();
+        }
+    }
+}
