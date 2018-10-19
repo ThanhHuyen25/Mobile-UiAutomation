3e8721bc3ebdee10c5cb6a6b73277eed4764e489
thanhhuyen <cattyhuyen@gmail.com>
2018-10-19 15:44:16 +0700

    AbstractAndroid

--- GUI_Testing_Automation_CL/Model/UIElements/Mobile/Android/AbstractAndroidElement.cs
+++ GUI_Testing_Automation_CL/Model/UIElements/Mobile/Android/AbstractAndroidElement.cs
@@ -14,5 +14,29 @@ namespace GUI_Testing_Automation
     /// </summary>
     public abstract class AbstractAndroidElement : ElementBase
     {
+        public static log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public const string LAYOUT = "layout";
+        public const string VIEW = "view";
+        public const string IMAGE_VIEW = "image view";
+        public const string TEXT_VIEW = "text view";
+        public const string VIEW_SWITCHER = "view switcher";
+        public const string BUTTON = "button";
+        public const string VIEW_PAGER = "view pager";
+        public const string EDIT_TEXT = "edit text";
+        public const string IMAGE_BUTTON = "image button";
+        public const string SPINNER = "spinner";
+        public const string LIST_VIEW = "list view";
+        public const string SCROLL_VIEW = "scroll view";
+
+
+        public AbstractAndroidElement(string id) : base(id)
+        {
+        }
+        public AbstractAndroidElement() : base()
+        {
+        }
+
+
     }
 }
